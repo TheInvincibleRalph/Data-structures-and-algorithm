6e6e8b55929ea6e5954f66a7c69d35d9de54542d
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-07-18 18:42:46 +0100

    initial commit

--- Algorithm/Sorting algorithm/insertion.go
+++ Algorithm/Sorting algorithm/insertion.go
@@ -4,11 +4,13 @@ func insertionSort(arr []int) {
 	n := len(arr)
 	for i := 1; i < n; i++ {
 		key := arr[i]
-		j := i - 1                   //hole
+		j := i - 1
 		for j >= 0 && arr[j] > key { //for every j greater than or equal to zero and the value of j is greater than the current value of i
-			arr[j+1] = arr[j] //move the value in j to the right by moving the (j+1)th value to the left
-			j--               //repeat the same recursive swap as long as the condition permits
+			arr[j+1] = arr[j] //Shift arr[j] to the right: updates the (j+1)th value to be the j-th value
+			j--               //this will ensure the (j+1)th value (the one in the hole) above keeps moving left until no number is greater than it is.
 		}
-		arr[j+1] = key //once the condition is exhausted, that is, the j-th value in the array is now less than the current i-th value, the least of the values is left in the (j+1)th position, it then replaces the current key
+		arr[j+1] = key // since key was replaced with the j-th value this inserts it back into the j-th position (check concept.md for simulation)
+		//once the nested for loop above breaks out, the key moves to the next index and the operation is performed all over again
 	}
+
 }
