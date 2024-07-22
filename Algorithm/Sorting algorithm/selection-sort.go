315ca286e41709c2af75e35f29c51e02e5ea0c11
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-07-22 12:57:00 +0100

    initial commit

--- Algorithm/Sorting algorithm/selection-sort.go
+++ Algorithm/Sorting algorithm/selection-sort.go
@@ -8,14 +8,14 @@ func selectionSort(arr []int) {
 	n := len(arr)
 
 	for i := 0; i < n-1; i++ {
-		minIndex := i //initializes the minimum index to be the current index of i
+		minVal := i //initializes the index of the minimum value to be the current index of i
 		for j := i + 1; j < n; j++ {
-			if arr[j] < arr[minIndex] { //condition to check if the value at index j is less than the initial value at index minIndex
-				minIndex = j //updates the minimum index to be the current index as approved by the if condition.
+			if arr[j] < arr[minVal] { //condition to check if the value at index j is less than the initial value at index minIndex
+				minVal = j //updates the minVal index to be the current index as approved by the if condition.
 			}
 
 		}
-		arr[i], arr[minIndex] = arr[minIndex], arr[i] //\swap the elements or values in the indexes
+		arr[i], arr[minVal] = arr[minVal], arr[i] //\swap the elements or values in the indexes
 	}
 }
 
@@ -27,10 +27,10 @@ func main() {
 }
 
 /*
-On using minIndex
+On using minVal
 
-Using minIndex (or a similar variable) correctly maintains the state
-of the smallest index or value found so far during iterations.
+Using minVal (or a similar variable) correctly maintains the state
+of the smallest value found so far during iterations.
 Using i instead would lead to incorrect comparisons and results,
 as i changes with each iteration and doesn't preserve the required
 state for finding the minimum or maximum element efficiently.
