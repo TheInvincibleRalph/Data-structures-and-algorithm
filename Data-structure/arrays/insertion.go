124e507a35cbf0c2c7718257d91ef09198db5a07
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-07-23 07:49:19 +0100

    initial commit

--- Data-structure/arrays/insertion.go
+++ Data-structure/arrays/insertion.go
@@ -9,8 +9,8 @@ func insertion(arr []int, index int, value int) []int {
 	}
 
 	sliceBeforeIndex := arr[:index] //this creates a slice of array that contains elements before the index (the element at the index exclusive)
+	sliceAfterIndex := arr[index:]  //this creates a slice of array that contains elements after the array (the element at the index inclusive)
 	// fmt.Println(sliceBeforeIndex)
-	sliceAfterIndex := arr[index:] //this creates a slice of array that contains elements after the array (the element at the index inclusive)
 	// fmt.Println(sliceAfterIndex)
 
 	arr = append(sliceBeforeIndex, append([]int{value}, sliceAfterIndex...)...) //an array is first created internally with the slice of elements after the index appended to the element at the given index, the result is afterwards appended to the slice before the index
