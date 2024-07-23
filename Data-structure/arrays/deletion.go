a09968527d1d6c438e47d2d34f1bacba76fe1180
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-07-23 07:57:52 +0100

    initial commit

---
+++ Data-structure/arrays/deletion.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func deletion(arr []int, index int) []int {
+	if index < 0 || index >= len(arr) {
+		fmt.Println("Index out of bound")
+		return arr
+	}
+
+	arr = append(arr[:index], arr[index+1:]...)
+	return arr
+}
+
+func main() {
+	arr := []int{10, 20, 30, 40, 50}
+	arr = deletion(arr, 2)
+	fmt.Println("Array after insertion:", arr)
+}
