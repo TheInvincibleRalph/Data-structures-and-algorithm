56b7a406bb01db53d354116a29b1754f1e05d33b
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-07-24 16:10:45 +0100

    initial commit

--- Data-structure/linked-lists/singly.go
+++ Data-structure/linked-lists/singly.go
@@ -13,10 +13,26 @@ type LinkedList struct {
 	head *Node //the root node defined as "head" contains a data and a pointer to the next node
 }
 
+//adds element at the start of a linked list
 func (list *LinkedList) addElementAtStart(data int) {
 	newNode := &Node{data: data} //returns the memory addressnof Node struct so that the newNode variable can modify the struct directly
 	newNode.next = list.head     //instantiate the original root to be the next in the list
 	list.head = newNode          //replaces root node with the new node
+}
+
+//adds element at the end of a linkedlist
+func (list *LinkedList) addElementAtEnd(data int) {
+	newNode := &Node{data: data}
+	current := list.head
+
+	if list.head == nil {
+		list.head = newNode
+		return
+	}
+	for current.next != nil {
+		current = current.next //the node originally pointed to by current is passed now to newNode
+	}
+	current.next = newNode //current.next points to newNode
 }
 
 // PrintList prints all elements in the list
@@ -33,6 +49,11 @@ func main() {
 	list := LinkedList{}
 
 	list.addElementAtStart(10)
+	list.addElementAtStart(0)
+	list.addElementAtEnd(20)
+	list.addElementAtEnd(30)
+	list.addElementAtEnd(40)
+	list.addElementAtEnd(50)
 
 	fmt.Println("Linked List:")
 	list.PrintList()
