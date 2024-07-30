3426301f7cba44d029c340bfbeef225222c8bc14
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-07-30 07:32:27 +0100

    initial commit

--- Data-structure/stack/stack.go
+++ Data-structure/stack/stack.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Stack struct {
 	items []int
 }
@@ -7,4 +9,27 @@ type Stack struct {
 // this adds an item to the stack
 func (s *Stack) push(item int) {
 	s.items = append(s.items, item)
+}
+
+func (s *Stack) pop() int {
+	if len(s.items) == 0 {
+		fmt.Println("Stack is empty")
+		return -1
+	}
+	item := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return item
+}
+
+func main() {
+	stack := Stack{}
+
+	stack.push(5)
+	stack.push(10)
+	stack.push(15)
+	stack.push(20)
+	stack.push(25)
+	stack.pop()
+
+	fmt.Println(stack)
 }
