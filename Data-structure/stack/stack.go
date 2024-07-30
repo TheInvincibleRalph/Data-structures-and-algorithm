48f800cb392c0e154d04c1058536de916def110b
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-07-30 07:59:07 +0100

    initial commit

--- Data-structure/stack/stack.go
+++ Data-structure/stack/stack.go
@@ -22,12 +22,18 @@ func (s *Stack) pop() int {
 	return item
 }
 
+//returns the top item of the stack without removing it
 func (s *Stack) peek() int {
 	if len(s.items) == 0 {
 		fmt.Println("Stack is empty")
 		return -1
 	}
 	return s.items[len(s.items)-1]
+}
+
+//isEmpty checks if the stack is empty
+func (s *Stack) isEmpty() bool {
+	return len(s.items) == 0
 }
 
 func main() {
@@ -42,5 +48,6 @@ func main() {
 	fmt.Println(stack.pop())
 	fmt.Println(stack)
 	fmt.Println(stack.peek())
+	fmt.Println(stack.isEmpty())
 
 }
