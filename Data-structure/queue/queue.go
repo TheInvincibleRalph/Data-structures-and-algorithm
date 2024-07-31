6f609e711301a12b212d9f2b65a026fc0fca9a76
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-07-31 04:45:39 +0100

    initial commit

--- Data-structure/queue/queue.go
+++ Data-structure/queue/queue.go
@@ -4,12 +4,12 @@ type Queue struct {
 	items []int
 }
 
-//enqueue adds an item to the queue
+// enqueue adds an item to the queue
 func (q *Queue) enqueue(item int) {
 	q.items = append(q.items, item)
 }
 
-//dequeue removes the first item added
+// dequeue removes the first item added
 func (q *Queue) dequeue() int {
 	if len(q.items) == 0 {
 		return -1
@@ -17,4 +17,9 @@ func (q *Queue) dequeue() int {
 	item := q.items[0]
 	q.items = q.items[1:]
 	return item
+}
+
+// isEmpty checks if the queue is empty
+func (q *Queue) isEmpty() bool {
+	return len(q.items) == 0 //returns true if the length of the queue is 0. This syntax is idiomatic for booleans in Go
 }
