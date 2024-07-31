4d8045d1b3d8c7bbe9f4e58027b4482ba88611a7
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-07-31 05:47:45 +0100

    initial commit

--- Data-structure/queue/deque.go
+++ Data-structure/queue/deque.go
@@ -11,8 +11,13 @@ func (d *Deque) addFront(item int) {
 
 //OR
 
-// func (d *Deque) addFront(item int) {
-// 	d.items = append(d.items, 0) // add a new element to the end of the slice to make space for the new item
-// 	copy(d.items[1:], d.items) // shift all elements to the right by one thereby duplicating the first element
-// 	d.items[0] = item // add the new item to the first index
-// }
+func (d *Deque) anotherAddFront(item int) {
+	d.items = append(d.items, 0) // add a new element to the end of the slice to make space for the new item
+	copy(d.items[1:], d.items)   // shift all elements to the right by one thereby duplicating the first element
+	d.items[0] = item            // add the new item to the first index
+}
+
+// add an item to the back of the deque
+func (d *Deque) addBack(item int) {
+	d.items = append(d.items, item)
+}
