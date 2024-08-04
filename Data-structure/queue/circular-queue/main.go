1bc0d3dff5b923978a4f12f904b117edae44afb6
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-08-04 10:09:50 +0100

    initial commit

--- Data-structure/queue/circular-queue/main.go
+++ Data-structure/queue/circular-queue/main.go
@@ -16,4 +16,12 @@ func NewCircularQueue(capacity int) *CircularQueue {
 	}
 }
 
+func (cq *CircularQueue) IsEmpty() bool {
+	return cq.front == cq.rear
+}
+
+func (cq *CircularQueue) IsFull() bool {
+	return (cq.rear+1)%cq.capacity == cq.front
+}
+
 //when a circular queue is instantiated, it returns a pointer to a CircularQueue struct with with an added slot to the capacity the queue was instantiated with
