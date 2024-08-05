5731320a0d9f5ccb50eeabb3ef20c4682f442b63
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-08-05 12:45:46 +0100

    initial commit

--- Data-structure/queue/circular-queue/main.go
+++ Data-structure/queue/circular-queue/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type CircularQueue struct {
 	items    []int
 	front    int
@@ -33,4 +35,42 @@ func (cq *CircularQueue) Enqueue(item int) bool {
 	return true
 }
 
-//when a circular queue is instantiated, it returns a pointer to a CircularQueue struct with with an added slot to the capacity the queue was instantiated with
+func (cq *CircularQueue) Dequeue() (int, bool) {
+	if cq.IsEmpty() {
+		return 0, false
+	}
+	item := cq.items[cq.front]              //cq.front is always pointing to the first element of the queue
+	cq.front = (cq.front + 1) % cq.capacity //cq.front is updated to the next element in the queue
+	return item, true
+}
+
+func (cq *CircularQueue) Front() (int, bool) {
+	if cq.IsEmpty() {
+		return 0, false
+	}
+	return cq.items[cq.front], true
+}
+
+func (cq *CircularQueue) Rear() (int, bool) {
+	if cq.IsEmpty() {
+		return 0, false
+	}
+	return cq.items[(cq.rear+cq.capacity-1)%cq.capacity], true
+}
+
+func main() {
+	cq := NewCircularQueue(5) //when a circular queue is instantiated, it returns a pointer to a CircularQueue struct with with an added slot to the capacity the queue was instantiated with
+
+	cq.Enqueue(1)
+	cq.Enqueue(2)
+	cq.Enqueue(3)
+	cq.Enqueue(4)
+	cq.Enqueue(5)
+	cq.Enqueue(6) //this will return false as the queue is full
+	cq.Dequeue()
+	cq.Enqueue(6) //this will return true as the queue has an empty slot after the last element
+
+	for !cq.IsEmpty() {
+		fmt.Println(cq.Dequeue())
+	}
+}
