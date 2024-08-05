e25dd2d0d8a21f67074b2796657339923c6b0bcb
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-08-05 12:36:53 +0100

    initial commit

--- Data-structure/queue/circular-queue/main.go
+++ Data-structure/queue/circular-queue/main.go
@@ -24,4 +24,13 @@ func (cq *CircularQueue) IsFull() bool {
 	return (cq.rear+1)%cq.capacity == cq.front
 }
 
+func (cq *CircularQueue) Enqueue(item int) bool {
+	if cq.IsFull() {
+		return false
+	}
+	cq.items[cq.rear] = item              //cq.rear is always pointing to the next empty slot
+	cq.rear = (cq.rear + 1) % cq.capacity //cq.rear is updated to the next empty slot within the capacity of the queue
+	return true
+}
+
 //when a circular queue is instantiated, it returns a pointer to a CircularQueue struct with with an added slot to the capacity the queue was instantiated with
